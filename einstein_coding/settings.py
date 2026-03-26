74c79780977fe9e79b0fe700bf2f47780a22c355
mariatmcosta <dudatmcosta@icloud.com>
2026-03-25 21:17:47 -0300

    debug false

--- einstein_coding/settings.py
+++ einstein_coding/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kjz-bi#jvxd*25u0(ukxbwtr8v&h0qp-tct*($cu2^q$!qf=ci'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
