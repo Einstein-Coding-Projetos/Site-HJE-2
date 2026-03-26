706838e2bf12274dfc9d560aec2c26971e87b5fa
mariatmcosta <dudatmcosta@icloud.com>
2026-03-25 21:52:31 -0300

    allowed hosts

--- einstein_coding/settings.py
+++ einstein_coding/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kjz-bi#jvxd*25u0(ukxbwtr8v&h0qp-tct*($cu2^q$!qf=ci
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
