317b1d132069758745cb46609d56528a06675b3a
Henrique Machiaveli Martins <hnriquemartins1406@gmai.com>
2026-03-27 09:15:06 -0300

    fix card CS

--- core/views.py
+++ core/views.py
@@ -28,13 +28,6 @@ def sobre_nos(request):
 def servicos(request):
     return render(request, 'servicos.html')
 
-def codigo_social(request):
-    # Agora puxa especificamente da tabela do Código Social
-    cards = CodigoSocialCard.objects.filter(is_active=True)
-    return render(request, 'codigo_social.html', {
-        'cards': cards
-    })
-
 def news_detail(request, id):
     news = get_object_or_404(News, id=id)
     return render(request, 'news_detail.html', {'news': news})
@@ -59,9 +52,6 @@ def noticias(request):
         )
     return render(request, 'noticias.html', {'noticias': noticias_list})
 
-def gestao(request):
-    return render(request, "gestao.html")
-
 def codigo_loja(request):
     products = Product.objects.filter(is_active=True)
     return render(request, 'loja.html', {
@@ -79,5 +69,10 @@ def gestao(request):
     return render(request, 'gestao.html', context);
 
 def codigo_social(request):
+    # Agora puxa os cards ativos e as fotos
+    cards = CodigoSocialCard.objects.filter(is_active=True)
     fotos = CodigoSocialFoto.objects.all()
-    return render(request, "codigo_social.html", {"fotos": fotos})
+    return render(request, "codigo_social.html", {
+        "cards": cards,
+        "fotos": fotos
+    })
