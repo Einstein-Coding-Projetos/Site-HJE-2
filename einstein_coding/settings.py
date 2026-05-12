829093252131fa6a5ed1cee47b6a05abb61c61bd
giulia mello <giuliamello@MacBook-Air-de-giulia.local>
2026-05-11 22:55:54 -0300

    alteracao de design e fonte

--- einstein_coding/settings.py
+++ einstein_coding/settings.py
@@ -106,7 +106,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
